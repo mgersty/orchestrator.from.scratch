249f0a49a52104ce8de62c994b91c01f65a43c7d
Matthew Gerstemeier <mgerstem@gmail.com>
2024-12-12 20:21:14 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module cube
+
+go 1.23.4
+
+require (
+	github.com/docker/go-connections v0.4.0
+	github.com/google/uuid v1.2.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
---
+++ worker/worker.go
@@ -0,0 +1,28 @@
+package worker
+
+import (
+	"cube/task"
+	"fmt"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+func (w *Worker) RunTask() {
+	fmt.Println("I will collect stats")
+}
+func (w *Worker) StartTask() {
+	fmt.Println("I will collect stats")
+}
+func (w *Worker) StopTask() {
+	fmt.Println("I will collect stats")
+}
